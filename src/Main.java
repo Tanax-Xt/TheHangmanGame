835cfe9e1d2cbfd03e81c28194d0c5bc8508bbd3
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2024-12-03 13:22:53 +0300

    create session prototype

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import game.Session;
 import words.HiddenWord;
 
 public class Main {
     public static void main(String[] args) {
-        HiddenWord word = new HiddenWord("Testggg");
-        System.out.println(word.getWord());
-        System.out.println(word.isInWord('g'));
-        word.openLetters('a');
-        System.out.println(word.getWord());
-        word.openLetters('g');
-        System.out.println(word.getWord());
+//        HiddenWord word = new HiddenWord("Testggg");
+//        System.out.println(word.getWord());
+//        System.out.println(word.isInWord('g'));
+//        word.openLetters('a');
+//        System.out.println(word.getWord());
+//        word.openLetters('g');
+//        System.out.println(word.getWord());
+        Session session = new Session();
+        System.out.println(session.getWord());
     }
 }
--- src/game/Session.java
+++ src/game/Session.java
@@ -1,4 +1,39 @@
 package game;
 
+import words.HiddenWord;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Session {
+    private final int maxTries = 6;
+    private final HiddenWord word = new HiddenWord(getRandomWord());
+
+    public Session() {
+    }
+
+    public int getMaxTries() {
+        return maxTries;
+    }
+
+    private String getRandomWord() {
+        List<String> wordsList;
+        try {
+            wordsList = Files.lines(Paths.get("src/game/words.txt")).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
+        Random random = new Random();
+        int index = random.nextInt(wordsList.size());
+        return wordsList.get(index);
+    }
+
+    public String getWord() {
+        return this.word.getWord();
+    }
 }
