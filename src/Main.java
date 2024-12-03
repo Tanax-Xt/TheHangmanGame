19fb4e64e562d0aa78ea14b712818692ff425963
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2024-12-04 00:39:54 +0300

    create and run game

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
+import game.ConsolePlay;
 import game.Session;
 
 public class Main {
     public static void main(String[] args) {
-//        HiddenWord word = new HiddenWord("Testggg");
-//        System.out.println(word.getWord());
-//        System.out.println(word.isInWord('g'));
-//        word.openLetters('a');
-//        System.out.println(word.getWord());
-//        word.openLetters('g');
-//        System.out.println(word.getWord());
-        Session session = new Session();
-        System.out.println(session.isUsed('d'));
-        System.out.println(session.getWord());
+        ConsolePlay consolePlay = new ConsolePlay();
+        consolePlay.run();
     }
 }
